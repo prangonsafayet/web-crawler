916d4c9af194d361d32b064d2e75aaa74e7e153f
safayetprangon <prangonsafayet@gmail.com>
2025-07-07 19:53:23 +0200

    created db tables through migration

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"os"
 
 	"github.com/safayetprangon/web-crawler/backend/config"
+	"github.com/safayetprangon/web-crawler/backend/models"
 )
 
 func main() {
-	// Initialize DB connection
 	config.ConnectDatabase()
 
-	// Optional test query using GORM to fetch current time from MySQL
-	var now string
-	result := config.DB.Raw("SELECT NOW()").Scan(&now)
-	if result.Error != nil {
-		log.Fatalf("❌ Failed test query: %v", result.Error)
-	}
+	// Auto-migrate tables
+	config.DB.AutoMigrate(&models.URL{}, &models.Result{}, &models.Link{})
 
-	fmt.Println("🕒 Current time from DB:", now)
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "8080"
+	}
 }
